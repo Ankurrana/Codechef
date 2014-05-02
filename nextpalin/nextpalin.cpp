ff0579526b284371998686ca0bd88868e15fa1ad
Ankur rana <ankurofficial@hotmail.com>
2014-05-02 15:23:52 +0530

    ladder

---
+++ nextpalin/nextpalin.cpp
@@ -0,0 +1,17 @@
+// string::length
+#include <iostream>
+#include <string>
+
+using namespace std;
+typedef long long int lld;
+
+int main (){
+	lld len = 0;
+	lld i,j,k,l;
+  string str;
+  cin >> str;
+
+  len = str.length();
+
+  return 0;
+}
